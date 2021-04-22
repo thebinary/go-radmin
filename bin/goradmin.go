93bafc1541d901ab6c1abd726699da62dfeaa38a
thebinary <binary4bytes@gmail.com>
2021-04-23 02:23:30 +0545

    add support on goradmin for socket type
    socket type and address flags

--- bin/goradmin.go
+++ bin/goradmin.go
@@ -10,37 +10,40 @@ import (
 )
 
 var sockAddr string
+var sockType string
 var command string
 
 func main() {
-	flag.StringVar(
-		&sockAddr, "f",
-		"/usr/local/var/run/radiusd/radiusd.sock",
-		"Radiusd control socket file",
-	)
 	flag.StringVar(
 		&command, "e",
 		"show uptime",
 		"Command to execute",
 	)
+	flag.StringVar(
+		&sockAddr, "a",
+		"/usr/local/var/run/radiusd/radiusd.sock",
+		"Radiusd control socket",
+	)
+	flag.StringVar(
+		&sockType, "p",
+		"unix",
+		"socket type",
+	)
 
 	flag.Parse()
 
-	r, err := libradmin.NewRadminClient(sockAddr)
+	r, err := libradmin.NewRadminClientWithConn(sockType, sockAddr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer r.Close()
 
-	p := make([]byte, 256)
-
-	r.Write([]byte(command))
-	for true {
-		n, _ := r.Read(p)
-		if int(r.LastReadChannel()) == libradmin.FR_CHANNEL_CMD_STATUS {
-			break
-		}
-		fmt.Println(string(p[:n-1]))
+	result, _, err := r.Execute([]byte(command))
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, line := range result {
+		fmt.Println(string(line))
 	}
 	os.Exit(int(r.LastReadStatus()))
 }
